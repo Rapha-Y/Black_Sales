526512005683b55ceba1f33c6c610ab735e00320
Rapha-Y <raph.yuki@gmail.com>
2021-01-05 10:54:13 -0300

    integrate product list front and back

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,50 +1,47 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import Product from './Product';
 
-const tempList = [
-    {
-        id: 1,
-        img: 'https://img.youtube.com/vi/pc9ptF7A5ec/maxresdefault.jpg',
-        title: 'Trident',
-        price: 2495
-    },
-    {
-        id: 2,
-        img: 'https://img.youtube.com/vi/NFQot-w_CrA/maxresdefault.jpg',
-        title: 'Drawing book',
-        price: 1299
-    },
-    {
-        id: 3,
-        img: 'https://img.youtube.com/vi/-sihZLgIO_k/maxresdefault.jpg',
-        title: 'Wine glass',
-        price: 2025
-    },
-    {
-        id: 4,
-        img: 'https://img.youtube.com/vi/pimxVr2fP9U/maxresdefault.jpg',
-        title: 'Magnifying glass',
-        price: 1895
-    },
-    {
-        id: 5,
-        img: 'https://img.youtube.com/vi/noQOk93TwLM/maxresdefault.jpg',
-        title: 'Motor',
-        price: 35294
-    }
-];
-
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    async function getProducts() {
+        try {
+            const response = await fetch(
+                'http://localhost:5000/products'
+            );
+
+            const parseRes = await response.json();
+
+            setProducts(parseRes);
+        } catch(error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     return (
         <Fragment>
             <h2>--ProductList</h2>
             <Container>
                 <Row>
                     {
-                        tempList.map(product => 
-                            <Col xs={12} sm={6} md={4} lg={3} xl={2} style={{ marginTop: '15px', marginBottom: '15px' }}>
-                                <Product id={product.id} img={product.img} title={product.title} price={product.price} />
+                        products.length !== 0 &&
+                        products.map(product => 
+                            <Col 
+                                xs={12} sm={6} md={4} lg={3} xl={2} 
+                                style={{ marginTop: '15px', marginBottom: '15px' }}
+                                key={product.product_id}
+                            >
+                                <Product 
+                                    id={product.product_id} 
+                                    img={product.product_image} 
+                                    title={product.product_name} 
+                                    price={product.product_price} 
+                                />
                             </Col>
                         )
                     }
