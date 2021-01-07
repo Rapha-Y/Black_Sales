7c443d101ea92ae1a0655267915195933bf95f00
Rapha-Y <raph.yuki@gmail.com>
2021-01-07 10:54:50 -0300

    add get method for specific product

--- server/routes/products.js
+++ server/routes/products.js
@@ -14,4 +14,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await pool.query(
+            'SELECT * FROM product WHERE product_id = $1',
+            [id]
+        );
+
+        res.json(product.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json('Server error');
+    }
+});
+
 module.exports = router;
