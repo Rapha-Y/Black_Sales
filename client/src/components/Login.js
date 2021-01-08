306b82d7c175b0ff9e1b95eb45c8cfce2ea55c1b
Rapha-Y <raph.yuki@gmail.com>
2021-01-08 15:31:33 -0300

    add login page front-end

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,24 @@
 import React, { Fragment } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 const Login = () => {
     return (
         <Fragment>
-            <h1>Login</h1>
-            [Form]
+            <Container>
+                <Form>
+                    <Form.Group controlId='email'>
+                        <Form.Label>E-mail:</Form.Label>
+                        <Form.Control type='email' placeholder='Enter your e-mail' />
+                    </Form.Group>
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type='password' placeholder='Enter your password' />
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
         </Fragment>
     );
 };
