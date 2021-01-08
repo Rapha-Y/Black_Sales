d090e3a7f4f20959529b8a543b4fb3650817efae
Rapha-Y <raph.yuki@gmail.com>
2021-01-08 16:56:21 -0300

    add registration post method

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use('/products', require('./routes/products'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = 5000;
 app.listen(PORT, () => {
