5b5a9aee341f085612c90997f00b2be23a7ecd32
Rapha-Y <raph.yuki@gmail.com>
2021-01-09 14:42:10 -0300

    add login post method

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -22,7 +22,7 @@ router.post('/register', async (req, res) => {
 
         const newUser = await pool.query(
             'INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *',
-            [name, email, password]
+            [name, email, bcryptPassword]
         );
 
         const token = jwtGenerator(newUser.rows[0].user_id);
@@ -33,5 +33,33 @@ router.post('/register', async (req, res) => {
         res.status(500).send('Server error');
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await pool.query(
+            'SELECT * FROM users WHERE user_email = $1',
+            [email]
+        );
+
+        if (user.rows.length === 0) {
+            return res.status(401).json('No user is registered under this e-mail');
+        }
+
+        const passwordMatches = await bcrypt.compare(password, user.rows[0].user_password);
+
+        if (!passwordMatches) {
+            return res.status(401).json('Incorrect password');
+        }
+
+        const token = jwtGenerator(user.rows[0].user_id);
+
+        res.json({ token });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+    }
+})
 
 module.exports = router;
