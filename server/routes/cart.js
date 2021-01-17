c090dc26d53bc41d487b4c1ac62a65334cb638a7
Rapha-Y <raph.yuki@gmail.com>
2021-01-17 20:37:14 -0300

    add post method for adding an item to a cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
-//const pool = require('../db');
+const pool = require('../db');
+
+router.post('/item', async (req, res) => {
+    try {
+        const { cart_id, product_id } = req.body;
+
+        const newItem = await pool.query(
+            'INSERT INTO cart_product (cart_id, product_id) VALUES ($1, $2) RETURNING *',
+            [cart_id, product_id]
+        );
+
+        res.json(newItem.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json('Server error');
+    }
+});
 
 module.exports = router;
