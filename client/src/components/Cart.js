2738737f0e8baf92d0630911156a7e62a9142263
Rapha-Y <raph.yuki@gmail.com>
2021-01-17 21:29:29 -0300

    link cart product list to get cart method

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,38 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Container, ListGroup, Button } from 'react-bootstrap';
 
 import Header from './Header';
 import './Cart.css';
 
-const fakeData = [
-    {
-        name: 'Spoontaneously',
-        price: 1000
-    },
-    {
-        name: 'Fork You',
-        price: 2050
-    },
-    {
-        name: 'Knife to Meet You',
-        price: 1525
-    }
-];
-
 const Cart = ({ isAuth }) => {
+    const [products, setProducts] = useState([]);
+
+    async function getProducts() {
+        try {
+            const response = await fetch(
+                'http://localhost:5000/cart',
+                {
+                    method: 'GET',
+                    headers: {
+                        token: localStorage.token
+                    }
+                }
+            );
+
+            const parseRes = await response.json();
+
+            setProducts(parseRes);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     return (
         <Fragment>
             <Header isAuth={isAuth} />
             <Container>
                 <ListGroup>
                     {
-                        fakeData.map(product =>
-                            <ListGroup.Item className='cartItem'>
+                        products.length !== 0 &&
+                        products.map(product =>
+                            <ListGroup.Item key={product.product_id} className='cartItem'>
                                 <div>
-                                   {product.name}
+                                   {product.product_name}
                                 </div>
                                 <div>
-                                    U${(product.price / 100).toFixed(2)}
+                                    U${(product.product_price / 100).toFixed(2)}
                                 </div>
                             </ListGroup.Item>
                         )
