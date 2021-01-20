9ef343c304c4882066f54fc04f0b8471bb2a0718
Rapha-Y <raph.yuki@gmail.com>
2021-01-20 17:47:33 -0300

    add jumbotron to filter menu

--- client/src/components/FilterMenu.js
+++ client/src/components/FilterMenu.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
 import SearchBar from './SearchBar';
 import TagList from './TagList';
 
 const FilterMenu = ({ setSearchText, setCategory }) => {
     return (
         <Fragment>
-            <SearchBar setSearchText={setSearchText} />
-            <TagList setCategory={setCategory} />
+            <Jumbotron className='py-4'>
+                <SearchBar setSearchText={setSearchText} />
+                <TagList setCategory={setCategory} />
+            </Jumbotron>
         </Fragment>
     );
 };
