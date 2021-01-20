8b92266fdb48fe5827de53458301c753394fc4c1
Rapha-Y <raph.yuki@gmail.com>
2021-01-20 17:41:27 -0300

    add site logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,22 @@
 import React, { Fragment } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
+import Logo from '../icons/treasure.svg';
+
 const Header = ({ isAuth }) => {
     return (
         <Fragment>
             <Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
-                <Navbar.Brand href='/'>[Logo + Site name]</Navbar.Brand>
+                <Navbar.Brand href='/'>
+                    <img
+                        alt='treasure-shaped site logo'
+                        src={Logo}
+                        width='30'
+                        height='30'
+                        className='d-inline-block align-top mr-2'
+                    />
+                    Green Sails
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                 <Navbar.Collapse id='responsive-navbar-nav'>
                     <Nav className='ml-auto'>
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 I'll be filled later, thanks for the comprehension.
+
+Add somewhere (footer?) later:
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
