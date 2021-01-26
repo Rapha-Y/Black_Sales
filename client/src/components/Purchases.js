e39955bee0c6eb994a0b07ef370a5af631e6b74d
Rapha-Y <raph.yuki@gmail.com>
2021-01-26 17:38:37 -0300

    add display for no orders made

--- client/src/components/Purchases.js
+++ client/src/components/Purchases.js
@@ -48,28 +48,37 @@ const Purchases = ({ isAuth }) => {
             <Header isAuth={isAuth} />
             <Container className='pageBody'>
                 {
-                    purchases.length !== 0 &&
-                    purchases.map(purchase => 
-                        <div key={purchase.order_id} className='mt-4'>
-                            <span>Bought on {getDate(purchase.order_date)}</span>
-                            <ListGroup className='mt-2'>
-                                {
-                                    purchase.product_list.map(product => 
-                                        <ListGroup.Item 
-                                            key={product.product_id} 
-                                            className='orderCartItem'
-                                        >
-                                            <div>
-                                                {product.product_name}
-                                            </div>
-                                            <div>
-                                                U${(product.product_price / 100).toFixed(2)}
-                                            </div>
-                                        </ListGroup.Item>
-                                    )
-                                }
-                            </ListGroup>
-                        </div>    
+                    purchases.length === 0 ?
+                    (
+                        <Container className='zeroOrderContainer mb-5'>
+                            <h3 className='text-muted'>
+                                You haven't ordered anything yet! <a href='/'>Check out the store!</a>
+                            </h3>
+                        </Container>
+                    ) :
+                    (
+                        purchases.map(purchase => 
+                            <div key={purchase.order_id} className='mt-4'>
+                                <span>Bought on {getDate(purchase.order_date)}</span>
+                                <ListGroup className='mt-2'>
+                                    {
+                                        purchase.product_list.map(product => 
+                                            <ListGroup.Item 
+                                                key={product.product_id} 
+                                                className='orderCartItem'
+                                            >
+                                                <div>
+                                                    {product.product_name}
+                                                </div>
+                                                <div>
+                                                    U${(product.product_price / 100).toFixed(2)}
+                                                </div>
+                                            </ListGroup.Item>
+                                        )
+                                    }
+                                </ListGroup>
+                            </div>    
+                        )
                     )
                 }
             </Container>
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 Fix Log In/Profile flick upon page load (present on Header)
 Add buy now functionality
-Customize cart page for empty carts
 Add pagination to products displayed (?)
 
 Refactor code (last step)
