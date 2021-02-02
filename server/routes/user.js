b010ac6fc6f908b8f6cda1c6f33e5b3b1dfdf738
Rapha-Y <raph.yuki@gmail.com>
2021-02-01 21:13:45 -0300

    add put method for changing password

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const pool = require('../db');
 const authorization = require('../middleware/authorization');
+const bcrypt = require('bcrypt');
 
 router.get('/', authorization, async (req, res) => {
     try {
@@ -12,6 +13,38 @@ router.get('/', authorization, async (req, res) => {
         );
 
         res.json(user.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json('Server error');
+    }
+});
+
+router.put('/', authorization, async (req, res) => {
+    try {
+        const uid = req.user.id;
+        const { currentPassword, newPassword } = req.body;
+
+        const user = await pool.query(
+            'SELECT user_password FROM users WHERE user_id = $1',
+            [uid]
+        );
+
+        const passwordMatches = await bcrypt.compare(currentPassword, user.rows[0].user_password)
+
+        if (!passwordMatches) {
+            return res.status(401).json('Incorrect password');
+        }
+
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+        const bcryptPassword = await bcrypt.hash(newPassword, salt);
+
+        const newPassUser = await pool.query(
+            'UPDATE users SET user_password = $1 WHERE user_id = $2 RETURNING *',
+            [bcryptPassword, uid]
+        );
+
+        res.json(newPassUser.rows[0]);
     } catch (error) {
         console.log(error.message);
         res.status(500).json('Server error');
