d6539d0afc42597f337d547f300620f2eb2467ce
Rapha-Y <raph.yuki@gmail.com>
2021-02-04 14:41:54 -0300

    update footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <Fragment>
             <div className='bg-dark p-3 pageFooter'>
-                <span className='text-light'>Copyright &copy; 2021 ADD_MY_NAME_LATER</span>
+                <span className='text-light'>Copyright &copy; 2021 Rapha-Y</span>
                 <div className='text-light'>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             </div>
         </Fragment>
--- readme.md
+++ readme.md
@@ -1,6 +1,3 @@
-Optional:
-Add pagination to products displayed - Needs bigger test sample.
-
 Final touches:
 Refactor code (last step)
 Write readme (actual last step)
